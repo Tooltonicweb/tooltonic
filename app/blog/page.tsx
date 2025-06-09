40ba35711e34054b067fddeaf9ed00ed2f27b68c
Tooltonicweb <jyotishmayi@gmail.com>
2025-06-09 15:47:03 +0530

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -27,8 +27,7 @@ focus:outline-none focus:ring-2 focus:ring-blue-500 shadow-sm"
           <button className="absolute right-2 top-2 bg-blue-600 text-white 
 rounded-full w-10 h-10 flex items-center justify-center hover:bg-blue-700 t
  ransition"> 
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fil
- l="none" viewBox="0 0 24 24" stroke="currentColor"> 
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> 
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidt
  h={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /> 
             </svg> 
